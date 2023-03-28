d1ecf74f10f2bf1ac32889ad34192da9d650fd16
AmezingHacker <125231810+SahilSarang@users.noreply.github.com>
2023-03-28 18:04:25 +0530

    Update QuestionsDetails.jsx

--- client/src/Pages/Questions/QuestionsDetails.jsx
+++ client/src/Pages/Questions/QuestionsDetails.jsx
@@ -82,7 +82,7 @@ const QuestionsDetails=()=>
     const User=useSelector((state)=>(state.currentUserReducer))
     const location=useLocation()
     const Navigate=useNavigate()
-    const url='http://localhost:3000'
+    const url='https://stack-overflow-aarti.netlify.app'
     const handlePostAns=(e,answerLength)=>{
         e.preventDefault()
         if(User===null)
